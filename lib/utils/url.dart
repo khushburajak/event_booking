20472a1cef5897e07b20eb683b89371f763a01e0
khushburajak <khushburajak17@gmail.com>
2024-02-26 15:37:31 +0545

    message and Urls created

---
+++ lib/utils/url.dart
@@ -0,0 +1,43 @@
+// baseUrl for mobile
+// const baseUrl = "http://192.168.56.1:5000/";
+
+// baseUrl for Local Host
+const baseUrl = "http://10.0.2.2:5000/";
+
+// baseUrl for Test Case
+// const baseUrl = "http://localhost:5000/";
+
+// UserUrl for User Api
+const userUrl = "users/api/";
+const loginUrl = "authenticate";
+const registerUrl = "register";
+const forgotPasswordUrl = "reset-password";
+
+// CategoryUrl for Category Api
+const categoryUrl = "categories/api/";
+const getcategoryUrl = "categories";
+const createcategoryUrl = "add-category";
+
+// EventUrl for Event Api
+const eventUrl = "events/api/";
+const addEventUrl = "create-event/";
+const getEventUrl = "get-event/";
+const getEventByUserUrl = "get-userevent/";
+const updateEventUrl = "update-event/";
+const attendEventUrl = "attend-event";
+const attendingEventUrl = "get-attendee-event";
+const likeEventUrl = "like-event/";
+const likeCountUrl = "like-count/";
+
+// ProfileUrl for Profile Api
+const profileUrl = "profiles/api/";
+const createProfileUrl = "create-profile";
+const getProfileUrl = "my-profile";
+const updateProfileUrl = "update-profile";
+
+// StoryUrl for Story Api
+const storyUrl = "stories/api/";
+const addStoryUrl = "event-story-image-upload/";
+const getStoryUrl = "event-stories/";
+
+String? token;
