920151d73070468fe60579ddac75a5083e4caffc
khushburajak <khushburajak17@gmail.com>
2024-02-26 14:21:32 +0545

    created category models

---
+++ lib/models/category.dart
@@ -0,0 +1,23 @@
+// ignore: depend_on_referenced_packages
+import 'package:json_annotation/json_annotation.dart';
+part 'category.g.dart';
+
+@JsonSerializable()
+class Category {
+  @JsonKey(name: '_id')
+  String? id;
+
+  String? name;
+  String? image;
+
+  Category({
+    this.id,
+    this.name,
+    this.image,
+  });
+
+  factory Category.fromJson(Map<String, dynamic> json) =>
+      _$CategoryFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CategoryToJson(this);
+}
